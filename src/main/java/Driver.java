7728c6cd58a4525d8f8fba035f7bc4de38bae037
hbogaeus <henry.bogaeus@gmail.com>
2015-05-13 22:17:17 +0200

    Changed around the Driver, added meta Driver and UML

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,9 +3,10 @@ import simulator.Simulation;
 import java.util.Scanner;
 
 public class Driver {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        Population population;
 
         System.out.println("Use default values? [y / n]");
         String answer = scanner.nextLine();
@@ -13,7 +14,7 @@ public class Driver {
         if (answer.equals("n")){
             System.out.print("Size of population: ");
             int populationSize = scanner.nextInt();
-            Population population = new Population(populationSize);
+            population = new Population(populationSize);
 
             System.out.println("Probability of infection? (0 - 1)");
             population.setInfectionProbabilityPerDay(scanner.nextDouble());
@@ -34,15 +35,14 @@ public class Driver {
                 int y = scanner.nextInt();
                 population.placeSickPerson(x, y);
             }
+        } else {
+            population = new Population(50);
+            population.setInfectionProbabilityPerDay(0.5);
+            population.setMortalityProbabilityPerDay(0.0);
+            population.setMinDays(3);
+            population.setMaxDays(9);
+            population.placeSickPerson(24, 24);
         }
-
-        Population population = new Population(3);
-        population.setInfectionProbabilityPerDay(0.6);
-        population.setMortalityProbabilityPerDay(0.4);
-        population.setMinDays(3);
-        population.setMaxDays(9);
-        population.placeSickPerson(0, 0);
-        population.placeSickPerson(2, 2);
 
         scanner.close();
 
@@ -57,5 +57,6 @@ public class Driver {
         for (int i = 0; i < simulation.totalSimulationData.size(); i++)
             System.out.println("Day "  + i + ": " + simulation.totalSimulationData.get(i).toString());
 
+        System.out.print("Percent of population sick: " + simulation.totalSimulationData.getLast().getTotalPeopleSick() / population.getPopulation().length * 2);
     }
 }
---
+++ src/main/java/DriverDriver.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class DriverDriver {
+    /*
+    public static void main(String[] args) {
+        int howManyDrivers = 100;
+        double average = 0;
+        double infectionRate = 0.040;
+        LinkedList<Double> runs = new LinkedList<>();
+
+        for (int i = 0; i < howManyDrivers; i++) {
+            double result = Driver.drive(infectionRate);
+            average += result;
+            runs.add(result);
+        }
+
+        Collections.sort(runs);
+
+        runs.forEach(System.out::println);
+
+        System.out.println("Infection rate: " + infectionRate * 100 + "%");
+        System.out.println("Average: " + average / (double) howManyDrivers);
+    }*/
+}
